8b7d246c6779e9ca93a429d28f8557afcfa9010c
Paulo Matos <pmatos@igalia.com>
2026-05-04 18:00:37 +0000

    Revert "[refactor][scheduler] Move host async tracking into JobScheduler"
    This reverts commit 018b5e6e0e98e783efa3fc5786507f1527fc7be3.

--- src/interpreter/builtins/atomics.rs
+++ src/interpreter/builtins/atomics.rs
@@ -673,9 +673,8 @@ impl Interpreter {
                         let resolve_clone = resolve_fn.clone();
                         let timeout_ms = timeout;
                         let pending = interp.agent_async_completions.clone();
-                        let pending_jobs = interp.scheduler.pending_async_jobs_handle();
-                        let pending_promise_ids =
-                            interp.scheduler.pending_async_promise_ids_handle();
+                        let pending_jobs = interp.pending_async_jobs.clone();
+                        let pending_promise_ids = interp.pending_async_promise_ids.clone();
                         let promise_id = if let JsValue::Object(ref o) = promise_val {
                             o.id
                         } else {
@@ -684,7 +683,7 @@ impl Interpreter {
                         if promise_id != 0 {
                             pending_promise_ids.lock().unwrap().insert(promise_id);
                         }
-                        pending_jobs.fetch_add(1, Ordering::SeqCst);
+                        pending_jobs.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
                         std::thread::spawn(move || {
                             let (lock, cvar) = &*pair_clone;
                             let mut notified = lock.lock().unwrap();
@@ -729,11 +728,11 @@ impl Interpreter {
                                         &[result_val],
                                     );
                                     interp.gc_unroot_value(&resolve);
-                                    if promise_id != 0 {
-                                        pending_promise_ids.lock().unwrap().remove(&promise_id);
-                                    }
-                                    pending_jobs.fetch_sub(1, Ordering::SeqCst);
                                 }));
+                            if promise_id != 0 {
+                                pending_promise_ids.lock().unwrap().remove(&promise_id);
+                            }
+                            pending_jobs.fetch_sub(1, std::sync::atomic::Ordering::SeqCst);
                             completion_cvar.notify_one();
                         });
                     }
